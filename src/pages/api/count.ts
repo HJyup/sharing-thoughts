1dbda007046597b99e4d283a86ec2c9f6f7dd2f0
d_butov <danyil.butov@gmail.com>
2023-06-05 11:41:19 +0100

    fix: fixed count

--- src/pages/api/count.ts
+++ src/pages/api/count.ts
@@ -17,7 +17,6 @@ async function handle(req: NextApiRequest, res: NextApiResponse) {
 
     res.status(201).json(posts[0].posts.length);
   }
-  res.status(404).json('Does not logged');
 }
 
 export default handle;
