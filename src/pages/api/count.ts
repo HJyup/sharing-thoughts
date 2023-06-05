71e23e0be4c8d05c68378af2b23218e16856a477
d_butov <danyil.butov@gmail.com>
2023-06-05 11:36:25 +0100

    fix: fixed count

--- src/pages/api/count.ts
+++ src/pages/api/count.ts
@@ -3,7 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '@/pages/api/client';
 
 async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const email = req.query.email;
+  const { query } = req;
+  const email = query.email;
   if (typeof email === 'string') {
     const posts = await prisma.user.findMany({
       where: {
